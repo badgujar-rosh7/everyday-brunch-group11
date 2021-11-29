94d54b567e1e9133883d2245cf7da8827f52c3d0
unknown <akshay.sahasrabuddhe@tantra-gyan.com>
2021-11-29 03:19:25 -0500

    Menu page development

--- routes/index.js
+++ routes/index.js
@@ -7,6 +7,10 @@ const constructorMethod = (app) => {
     res.render('pages/index');
   });
 
+  app.get('/menu', (req, res) => {
+    res.render('pages/menu');
+  });
+
   app.use('*', (req, res) => {
     res.status(404).json({"Error" : "Resource Not Found"});
   });
