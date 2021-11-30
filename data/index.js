97917f3cacfcf35a3fc5731a1ff134ed23473fe0
Tanay Tadas <tanaysid97@gmail.com>
2021-11-30 01:15:24 -0500

    Few changes in login and index

--- data/index.js
+++ data/index.js
@@ -1,10 +1,8 @@
-//const menudata = require('./menu');
 const logindata = require('./login');
 const signupdata = require('./signup');
 // const reviewdata = require('./reviews');
 
 module.exports = {
-    menu: menudata,
     login: logindata,
     signup: signupdata,
     // reviews: reviewdata,
