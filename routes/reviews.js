69b21c6c44d5ebd51f5bc8259dfd098de5780e65
Tanay Tadas <tanaysid97@gmail.com>
2021-12-04 01:39:58 -0500

    few changes to review

--- routes/reviews.js
+++ routes/reviews.js
@@ -4,12 +4,20 @@ const data = require('../data');
 const path = require('path');
 const reviewData = data.reviews;
 
+const ErrorCode = {
+    BAD_REQUEST: 400,
+    NOT_FOUND: 404,
+    INTERNAL_SERVER_ERROR: 500,
+};
+router.post('/newreview', async (req, res) => {});
+router.get('/:id', async (req, res) => {});
+router.get('/review/:id', async (req, res) => {});
 router.get('/delete/:id', async (req, res) => {
     try {
         let deleteReview = await reviewData.removeReviewById(req.params.id);
         res.json(deleteReview);
     } catch (error) {
-        response.status(error.code || ErrorCode.INTERNAL_SERVER_ERROR).send({
+        res.status(error.code || ErrorCode.INTERNAL_SERVER_ERROR).send({
             serverResponse: error.message || 'Internal server error.',
         });
     }
