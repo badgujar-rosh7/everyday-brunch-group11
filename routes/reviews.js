3ebcad7c980c25cbca576fac704a3712561eb8ae
Tanay Tadas <tanaysid97@gmail.com>
2021-12-04 15:26:27 -0500

    added new routes for review

--- routes/reviews.js
+++ routes/reviews.js
@@ -9,9 +9,54 @@ const ErrorCode = {
     NOT_FOUND: 404,
     INTERNAL_SERVER_ERROR: 500,
 };
-router.post('/newreview', async (req, res) => {});
-router.get('/:id', async (req, res) => {});
-router.get('/review/:id', async (req, res) => {});
+router.post('/newreview', async (req, res) => {
+    try {
+        const { userId, review, rating } = req.body;
+        const current_datetime = new Date();
+        const formatted_date =
+            current_datetime.getMonth() +
+            1 +
+            '/' +
+            current_datetime.getDate() +
+            '/' +
+            current_datetime.getFullYear();
+        const createReview = await reviewData.createReview(
+            userId,
+            review,
+            rating,
+            formatted_date
+        );
+        res.json(createReview);
+    } catch (error) {
+        res.status(error.code || ErrorCode.INTERNAL_SERVER_ERROR).send({
+            serverResponse: error.message || 'Internal server error.',
+        });
+    }
+});
+router.get('/:id', async (req, res) => {
+    try {
+        const reviewByUserId = await reviewData.getAllReviewsByUserId(
+            req.params.id
+        );
+        res.json(reviewByUserId);
+    } catch (error) {
+        res.status(error.code || ErrorCode.INTERNAL_SERVER_ERROR).send({
+            serverResponse: error.message || 'Internal server error.',
+        });
+    }
+});
+router.get('/review/:id', async (req, res) => {
+    try {
+        const reviewByreviewId = await reviewData.getReviewByReviewId(
+            req.params.id
+        );
+        res.json(reviewByreviewId);
+    } catch (error) {
+        res.status(error.code || ErrorCode.INTERNAL_SERVER_ERROR).send({
+            serverResponse: error.message || 'Internal server error.',
+        });
+    }
+});
 router.get('/delete/:id', async (req, res) => {
     try {
         let deleteReview = await reviewData.removeReviewById(req.params.id);
