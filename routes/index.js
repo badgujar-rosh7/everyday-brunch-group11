03e9614c4ec6c6f062535ff73cea01874c4668aa
Tanay Tadas <tanaysid97@gmail.com>
2021-12-03 22:02:08 -0500

    few adjustments

--- routes/index.js
+++ routes/index.js
@@ -1,39 +1,47 @@
 const path = require('path');
-const adminRoutes=require('./admin')
+const adminRoutes = require('./admin');
+const userRoutes = require('./users');
+const reviewsRoutes = require('./reviews');
 
 const constructorMethod = (app) => {
     app.get('/', (req, res) => {
         res.render('pages/index');
     });
 
-  app.get('/menu', (req, res) => {
-    res.render('pages/menu',{pageHeading:"Menu"});
-  });
+    app.get('/menu', (req, res) => {
+        res.render('pages/menu', { pageHeading: 'Menu' });
+    });
 
-  app.get('/sides', (req, res) => {
-    res.render('pages/menu',{pageHeading:"Sides"});
-  });
+    app.get('/sides', (req, res) => {
+        res.render('pages/menu', { pageHeading: 'Sides' });
+    });
 
-  app.get('/beverages', (req, res) => {
-    res.render('pages/menu',{pageHeading:"Beverages"});
-  });
+    app.get('/beverages', (req, res) => {
+        res.render('pages/menu', { pageHeading: 'Beverages' });
+    });
 
-  app.get('/desserts', (req, res) => {
-    res.render('pages/menu',{pageHeading:"Desserts"});
-  });
+    app.get('/desserts', (req, res) => {
+        res.render('pages/menu', { pageHeading: 'Desserts' });
+    });
 
-  app.get('/cart', (req, res) => {
-    res.render('pages/cart');
-  });
+    app.get('/cart', (req, res) => {
+        res.render('pages/cart');
+    });
 
-/////////////////////////////////////////////////////Roshan
-app.use('/admin', adminRoutes);
+    /////////////////////////////////////////////////////Roshan
+    app.use('/admin', adminRoutes);
 
-/////////////////////////////////////////////////Roshan
+    /////////////////////////////////////////////////Roshan
 
-  app.use('*', (req, res) => {
-    res.status(404).json({"Error" : "Resource Not Found"});
-  });
+    /*******************************************************************************Tanay*/
+    app.use('/users', userRoutes);
+    app.use('/reviews', reviewsRoutes);
+    /*******************************************************************************Tanay*/
+
+    //
+    app.use('*', (req, res) => {
+        res.status(404).json({ Error: 'Resource Not Found' });
+    });
 };
 
 module.exports = constructorMethod;
