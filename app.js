9ed0fe80250c223ccd240392542c7ce74e94da53
Tanay Tadas <tanaysid97@gmail.com>
2021-12-04 22:51:41 -0500

    added middlewares

--- app.js
+++ app.js
@@ -1,30 +1,36 @@
 const express = require('express');
 const app = express();
 const static = express.static(__dirname + '/public');
-const session = require('express-session');
+const cookieParser = require('cookie-parser');
 const configRoutes = require('./routes');
 const exphbs = require('express-handlebars');
-
-const fileupload = require("express-fileupload");
+const session = require('express-session');
+const fileupload = require('express-fileupload');
 app.use(fileupload());
-
+app.use(cookieParser());
 app.use('/public', static);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+    session({
+        name: 'AuthCookie',
+        secret: 'secretKey',
+        saveUninitialized: true,
+        resave: false,
+        cookie: { maxAge: 600000 },
+    })
+);
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-app.use(
-    session({
-      name: 'AuthCookie',
-      secret: "secretKey",
-      saveUninitialized: true,
-      resave: false,
-      cookie: { maxAge: 600000 },
-    })
-  );
-
+app.use('/login', (req, res, next) => {
+    if (req.session.user) {
+        return res.redirect('/users/profile');
+    } else {
+        next();
+    }
+});
 
 configRoutes(app);
 
