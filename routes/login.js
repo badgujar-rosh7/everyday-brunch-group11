9fa9da0b7e1e56472df46aa0c6ad6e5bc59fa092
Tanay Tadas <tanaysid97@gmail.com>
2021-12-04 22:58:45 -0500

    added req session for login

--- routes/login.js
+++ routes/login.js
@@ -4,6 +4,7 @@ const data = require('../data');
 const loginData = data.login;
 const errorcheck = data.error;
 const xss = require('xss');
+const userdata = data.user;
 
 const ErrorCode = {
     BAD_REQUEST: 400,
@@ -11,23 +12,48 @@ const ErrorCode = {
     INTERNAL_SERVER_ERROR: 500,
 };
 router.get('/', async (req, res) => {
-    res.render('pages/loginform');
+    if (req.session.user) {
+        res.redirect('/');
+    } else {
+        res.render('pages/loginform');
+    }
 });
 router.post('/', async (req, res) => {
     try {
-        const username = xss(req.body.username.trim());
+        const username = xss(req.body.username.trim().toLowerCase());
         const password = xss(req.body.password.trim());
         const validatedUsername = errorcheck.validateUsername(username);
         const validatedPassword = errorcheck.validatePassword(password);
-        const checkuser = await loginData.checkUser(
-            validatedUsername,
-            validatedPassword
-        );
-        res.json(checkuser);
+
+        if (req.session.user) {
+            auth = 'Authorised User';
+            return res.redirect('/users/profile');
+        } else {
+            const checkuser = await loginData.checkUser(
+                validatedUsername,
+                validatedPassword
+            );
+            if (!checkuser.authenticated) {
+                throwError(
+                    ErrorCode.INTERNAL_SERVER_ERROR,
+                    'Internal Server Error'
+                );
+            }
+            const userId = await userdata.getUserId(validatedUsername);
+            req.session.user = { username: validatedUsername, userId: userId };
+            // req.session.user = user;
+            return res.redirect('users/profile');
+        }
     } catch (error) {
-        res.status(error.code || ErrorCode.INTERNAL_SERVER_ERROR).send({
-            serverResponse: error.message || 'Internal server error.',
-        });
+        res.status(error.code || ErrorCode.INTERNAL_SERVER_ERROR).render(
+            'pages/loginform',
+            {
+                title: 'Login',
+                hasErrors: true,
+                error: error.message || 'Internal Server Error',
+            }
+        );
     }
 });
+
 module.exports = router;
